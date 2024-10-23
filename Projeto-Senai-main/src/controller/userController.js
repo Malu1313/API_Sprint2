1bcc05f44727b2c380e8d5cd58aaa3b83d823564
Malu1313 <maluvinholireis02@gmail.com>
2024-10-23 16:19:11 -0300

    23-10

--- Projeto-Senai-main/src/controller/userController.js
+++ Projeto-Senai-main/src/controller/userController.js
@@ -1,10 +1,12 @@
 let users = []; //Array que armazena os usuários
+let id_usuario = 0;
+const connect = require("../db/connect");
 
 module.exports = class userController{
     static async createUser(req, res) {
-        const { nome, email, senha, telefone } = req.body;
+        const { nome, email, senha, telefone, tipo_de_usuario } = req.body;
 
-        if (!nome || !email || !senha || !telefone) {
+        if (!nome || !email || !senha || !telefone || !tipo_de_usuario) {
             //Verifica se todos os campos estão preenchidos
             return res
               .status(400)
@@ -20,81 +22,141 @@ module.exports = class userController{
             //Verifica se o email tem o @
             return res.status(400).json({ error: "Email inválido. Deve conter @" });
         }
-        // Verifica se já existe um usuário com o mesmo Email
-    const existingUser = users.find((user) => user.email === email);
-    if (existingUser) {
-      return res.status(400).json({ error: "Email já cadastrado" });
+        // Construção da query INSERT
+      const query = `INSERT INTO usuarios (nome, senha, telefone, tipo_de_usuario) VALUES('${nome}', '${senha}', '${telefone}', '${tipo_de_usuario}')`;
+      // Executando a query criada
+      try {
+        connect.query(query, function (err) {
+          if (err) {
+            console.log(err);
+            console.log(err.code);
+            if (err.code === "ER_DUP_ENTRY") {
+              return res
+                .status(400)
+                .json({ error: "O Email já está vinculado a outro usuário" });
+            } else {
+              return res
+                .status(500)
+                .json({ error: "Erro interno do servidor" });
+            }
+          } else {
+            return res
+              .status(201)
+              .json({ message: "Usuário criado com sucesso" });
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro interno do servidor" });
+      }
     }
-
-    const newUser = { id: id_usuario++, nome, email, senha, telefone  };    
-    users.push(newUser);
-    return res.status(201).json({ message: "Usuário criado com sucesso", user: newUser });
-}
-
+  }
     static async getAllUser(req, res) {
     //Lista todos os usuarios
-    return res
-      .status(200)
-      .json({ message: "Obtendo todos os usuários", users }); //200 significa sucesso
-  }
+    const query = `SELECT * FROM usuarios`;
+
+    try {
+      connect.query(query, function (err, results) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Erro interno do Servidor" });
+        }
+
+        return res
+          .status(200)
+          .json({ message: "Lista de usuários", users: results });
+      });
+    } catch (error) {
+      console.error("Erro ao executar a consulta:", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+    }
     static async updateUser (req, res){
     // Desestrutura e recupera os dados enviados via corpo da requisição
-    const { nome, email, senha, telefone } = req.body;
+    const { nome, email, senha, telefone, tipo_de_usuario } = req.body;
 
     // Validar se todos os campos foram preenchidos
-    if ( !nome || !email || !senha || !telefone) {
+    if ( !nome || !email || !senha || !telefone || !tipo_de_usuario) {
         return res
           .status(400)
           .json({ error: "Todos os campos devem ser preenchidos" });
       }
-    // Procurar o indice do organizador no Array 'organizadores' pelo id
-    const userIndex = users.findIndex((user) => user.id_usuario === id_usuario);
-
-    // Se o organizador não for encontrado organizadorIndex equivale a -1
-    if (userIndex === -1) {
-        return res.status(400).json({ error: "Usuário não encontrado" });
+      const query = `UPDATE usuarios SET nome=?,senha=?,telefone=?, tipo_de_usuario=? WHERE id_usuario = ?`;
+      const values = [ nome, senha, email, telefone , tipo_de_usuario, id_usuario];
+  
+      try {
+        connect.query(query, values, function (err, results) {
+          if (err) {
+            if (err.code === "ER_DUP_ENTRY") {
+              return res
+                .status(400)
+                .json({ error: "Email já cadastrado por outro usuario" });
+            } else {
+              console.error(err);
+              return res.status(500).json({ error: "Erro interno do servidor" });
+            }
+          }
+          if (results.affectedRows === 0) {
+            return res.status(404).json({ error: "usuario não encontrado" });
+          }
+          return res
+            .status(200)
+            .json({ message: "usuario foi atualizado com sucesso" });
+        });
+      } catch {
+        error;
       }
-      // Atualiza os dados do organizador do Array 'organizadores'
-    users[userIndex] = { id_usuario, nome, email, senha, telefone };
-    return res
-      .status(200)
-      .json({ message: "Usuário atualizado", user: users[userIndex] });
+      {
+        console.error("Erro ao executar consulta");
+        return res.status(500).json({ error: "Erro interno do servidor" });
+      }
     }
 
     static async deleteUser(req, res) {
         // Obtém o parâmetro 'id' da requisição, que é o id do usuário a ser deletado
-        const id_usuario = req.params.id_usuario;
-
-        // Procurar o indice do organizador no Array 'organizadores' pelo email
-    const userIndex = users.findIndex((user) => user.id_usuario == id_usuario);
-
-    // Se o usuário não for encontrado organizadorIndex equivale a -1
-    if (userIndex === -1) {
-        return res.status(400).json({ error: "Usuário não encontrado" });
+      const id_usuario = req.params.id_usuario;
+      const query = `DELETE FROM usuarios WHERE id_usuario = ?`;
+      const values = [id_usuario];
+  
+      try {
+        connect.query(query, values, function (err, results) {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Erro interno do servidor" });
+          }
+  
+          if (results.affectedRows === 0) {
+            return res.status(404).json({ error: "Usuario não encontrado" });
+          }
+  
+          return res
+            .status(200)
+            .json({ message: "Usuario exluido com sucesso" });
+        });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json
+          ({error:"Erro interno do servidor"})
       }
-
-      //Removendo o usuário do Array 'users'
-    users.splice(userIndex, 1);
-
-    return res.status(200).json({message: "Usuário Apagado"});
-}
-    // Função de login  
-    static async loginUser(req, res) {    
-        const { email, password } = req.body;
-    
-        if (!email || !password) {      
-            return res.status(400).json({ error: "Email e senha são obrigatórios" });    
-        }
-    const user = users.find((user) => user.email === email);
-    
-    if (!user) {
-          return res.status(400).json({ error: "Usuário não encontrado" });    
-        }
-    if (user.password !== password) {      
-        return res.status(400).json({ error: "Senha incorreta" });    
-    }
-    return res.status(200).json({ message: "Login realizado com sucesso", user });  
-}};
-
-
+    };
+    // Função de login
+  static async loginUser(req, res) {
+  const {nome, senha} = req.body;
+  if (|| !nome || !senha) {
+    return res.status(400).json({ error: "Email e senha são obrigatórios" });
+  }
+  
+  const user = users.find((user) => user.email === email);
+  
+  if (!users) {
+    return res.status(400).json({ error: "Usuário não encontrado" });
+  }
+  
+  if (user.senha !== senha) {
+    return res.status(400).json({ error: "Senha incorreta" });
+  }
+  
+  return res.status(200).json({ message: "Login realizado com sucesso", users });
+}; 
+ 
 
